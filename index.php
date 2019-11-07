f32fda852eb39d8693482ce08286b5b3b23caae9
Maciej Mniszak <mniszakmaciej@endor.local>
2019-11-07 12:14:13 +0100

    ok

--- index.php
+++ index.php
@@ -1,33 +1,61 @@
-<html>
-    <head>
-    </head>
-    <body>
+<?php
+    require_once('database.php');
     
-        <form action="" method="post" onsubmit="return addElements()">
-            <input id="inputA" type="text">
-            <input id="inputB" type="text">
-            aaa
-            <button  type="submit" id ="add">Dodaj</button>
-            <button id="stop">Wykonaj</button>
-        </form>
-    </body>
-    <script>
-    var aArray = [];
-    var bArray = [];
-    var addElements = function() {
-            var v = document.getElementById('inputA');
-            var b = document.getElementById('inputA');
-            aArray.push(v);
-            bArray.push(b);
-        }
-    var n = aArray.length;
-    var y = n - 1;
-    if(n == 5 || addEventListener('add') == true) {
-        for(let x = 0, x > n, x++;) {
-            echo = 'Dodawanie: ' + aArray[x] + bArray[y] + '<br>';
-            echo = 'Odejmowanie: ' + aArray[x] - bArray[y] + '<br>';
-            y--; 
-        }
-    }
-    </script>
+$send = 'a'.$_GET['send'];
+switch($send) {
+    case 'ateacher':
+        $db_conn = dbConnect($db_server,$db_name,$db_user,$db_pass);
+    break;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php?send=teacher" method="post" name="addTeacher"id="addTeacher">
+        <input type="text" name="imie" id="">
+        <input type="text" name="nazwisko" id="">
+        <input type="text" name="pesel" id="">
+        <input type="submit" value="">
+        
+    </form>
+</body>
 </html>
+<?php
+$db_server = 'localhost';
+$db_user = 'zsht';
+$db_pass = '1234';
+$db_name = 'zsht';
+
+function dbConnect($db_server,$db_name,$db_user,$db_pass){
+try {
+    $db_conn = new PDO('mysql:host='.$db_server.';dbname='.$db_name, $db_user, $db_pass);
+    $db_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    echo $e->getMessage();
+}
+return $db_conn;
+}
+    
+    $db_query = $db_conn->query('select * from uczniowie');
+    foreach($db_query as $row){
+        echo'<div>';
+        echo $row['ID'];
+        echo ' - ';
+        echo $row['imie'];
+        echo ' ';
+        echo $row['nazwisko'];
+        echo '(';
+        echo $row['pesel'];
+        echo ')';
+        echo '</div>';
+    }
+
+
+?>
